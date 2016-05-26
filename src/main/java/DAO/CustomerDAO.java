fb419d24969c014ef3f3f3c2ed016fe3c0d62590
pischenkol <lina.pischenko@gmail.com>
2016-05-26 22:25:15 +0300

    change

--- src/main/java/DAO/CustomerDAO.java
+++ src/main/java/DAO/CustomerDAO.java
@@ -2,48 +2,72 @@ package DAO;
 
 import models.Customer;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Set;
+
 
 /**
  * Created by appleface on 26.05.16.
  */
 public class CustomerDAO {
-    private final Connection connection;
+    public final String url = "jdbc:mysql://192.168.33.10:3306/test";
+    public final String login = "root";
+    public final String password = "my-new-password";
 
-    public CustomerDAO(Connection connection) {
-        this.connection = connection;
-    }
-    public boolean save(Customer audios){
-        Statement statement = null;
-        try {
-            statement.execute("insert into CUSTOMERS(COMPANY, CUST_REP, CREDIT_LIMIT) values(%s,'%i','%d')");
+    public boolean save(Customer customer) {
+        Boolean bl = false;
+        try (Connection connection = DriverManager.getConnection(url,login,password)) {
+            Statement st = connection.createStatement();
+            bl = st.execute("insert into CUSTOMERS(COMPANY, CUST_REP, CREDIT_LIMIT) values(%s,'%i','%d')");
+            return bl;
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
 
-        return true;}
-    public boolean updete(Customer audios){return true;}
-    public boolean delede(Customer audios){return true;}
-    public Customer getById(int id){
-        return null;
+        return bl;
     }
-    public Set<Customer> getAll(){
-        return null;
+
+    public boolean update(Customer customer) {
+        int bl = 0;
+        try (Connection connection = DriverManager.getConnection(url,login,password)) {
+            Statement st = connection.createStatement();
+            bl = st.executeUpdate("insert into CUSTOMERS(COMPANY, CUST_REP, CREDIT_LIMIT) values(%s,'%i','%d')");
+            return bl!=0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        return false;
     }
-    public Set<Customer> get2103(){
+
+    public boolean delete(Customer customer) {
+        return true;
+    }
+
+    public Customer getById(int id) {
         return null;
     }
 
+    public Set<Customer> getAll() {
+        return null;
+    }
+
+    public Set<Customer> get2103() {
+        return null;
+    }
 
 
     private boolean executeSql(String sql) {
         int count = 0;
         Statement statement = null;
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url,login,password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         try {
             statement = connection.createStatement();
             count = statement.executeUpdate(sql);
